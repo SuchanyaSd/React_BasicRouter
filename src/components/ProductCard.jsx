a81a36ab1a7ffd9eece91b8afe10103344bf405d
SuchanyaSd <suchanya_sd@hotmail.com>
2025-01-22 16:52:10 +0700

    Done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,28 @@
-import { ShoppingCart } from 'lucide-react';
+import { ShoppingCart, Trash2} from 'lucide-react';
 import useProductStore from '../store/product-store';
 
-function ProductCard({product}) {
+function ProductCard({ product }) {
     const actionAddToCart = useProductStore((state) => state.actionAddToCart)
-  return (
-    <div className="bg-slate-100 m-2 p-2 rounded-md shadow-md flex flex-col justify-center w-[300px] h-[400px] text-center gap-5">
-      <div className='flex flex-col gap-3'>
-        <img src={product.images[0]}  className="w-[280px] h-[150px] object-cover"/>
-        <p className="text-sm font-semibold">{product.title}</p>
-        <p className="text-sm">{product.description}</p>
-      </div>
-      <div className="flex justify-between bg-cyan-900 text-white ">
-        {/* <h5>{product.category}</h5> */}
-        <p className="text-sm">Price : {product.price}</p>
-        <p><ShoppingCart
-        color='white'
-        onClick={()=>actionAddToCart(product)}/></p>
-      </div>
-    </div>
-  )
+    const actionRemoveProduct = useProductStore((state) => state.actionRemoveProduct)
+    return (
+        <div className="bg-slate-100 m-2 p-2 rounded-md shadow-md flex flex-col justify-center w-[300px] h-[400px] text-center gap-5">
+            <div className='flex flex-col gap-3'>
+                <img src={product.images[0]} className="w-[280px] h-[150px] object-cover" />
+                <p className="text-sm font-semibold">{product.title}</p>
+                <p className="text-sm">{product.description}</p>
+            </div>
+            <div className="flex justify-between bg-cyan-900 text-white ">
+                {/* <h5>{product.category}</h5> */}
+                <p className="text-sm">Price : {product.price}</p>
+                <div className='flex gap-3'>
+                    <button><Trash2 onClick={() => actionRemoveProduct(product.id)} color='red'/></button>
+                    <button><ShoppingCart
+                        color='white'
+                        onClick={() => actionAddToCart(product)} /></button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ProductCard
--- src/store/product-store.jsx
+++ src/store/product-store.jsx
@@ -6,6 +6,7 @@ import { persist } from "zustand/middleware"
 const productStore = (set) => ({
     products : [],
     cart : [],
+    
     actionGetData :  async () => {
         try {
             const res = await axios.get('https://dummyjson.com/products')
@@ -20,11 +21,22 @@ const productStore = (set) => ({
         // console.log(product);
         set((state)=>({cart:[...state.cart,product]}))
     },
+    actionRemoveProduct : (id) => {
+        set((state) => ({cart : state.cart.filter((item) => item.id !== id)}))
+    },
     actionClear : () => {
         set({cart : []})
-    }
+    },
 })
+const persistStore = {
+    name : 'product-store',
+    partialize : (state) => ({
+    cart : state.cart,
+    })
 
-const useProductStore = create(persist(productStore,{name : 'product-store'}))
+    
+}
+
+const useProductStore = create(persist(productStore,persistStore))
 
 export default useProductStore
